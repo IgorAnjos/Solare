cb06d13094bc867f7fa6420bb5357faa0acb0e52
Igor Anjos <igor.anjos@orbium.com.br>
2020-06-07 23:52:37 -0300

    chore: add entidades na classe para o EF

--- web/Data/ApplicationDbContext.cs
+++ web/Data/ApplicationDbContext.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using web.Models;
 
 namespace web.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        DbSet<AddressBirth> AddressBirths { get; set; }
+        DbSet<AddressCurrent> AddressCurrents { get; set; }
+        DbSet<Contact> Contacts { get; set; }
+        DbSet<Customer> Customers{ get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
