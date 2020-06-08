49458e3d5937199c3926f81606524a4845941d73
Igor Anjos <igor.anjos@orbium.com.br>
2020-06-07 23:51:54 -0300

    chore: metodo para salvar customer

--- web/Controllers/CustomerController.cs
+++ web/Controllers/CustomerController.cs
@@ -1,20 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
+using web.Data;
+using web.Models;
+using web.Models.DTO;
 
 namespace web.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly ApplicationDbContext database;
+
+        public CustomerController(ApplicationDbContext _applicationDbContext)
+        {
+            this.database = _applicationDbContext;
+        }
         public IActionResult Customers(){
             return View();
         }
 
-        public IActionResult CustomerAdd(){
+        public IActionResult CustomerAdd()
+        {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Save(){
-            return View();
+        public IActionResult Save(CustomerDTO _customerDTO)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                Customer customer = new Customer();
+
+                customer.FullName = _customerDTO.FullName;
+                customer.CustomerType = _customerDTO.Type;
+                //continuar
+
+                //database.Customers.Add(customer);
+                database.SaveChanges();
+                
+                return RedirectToAction("","");
+            }
+            else
+            {
+                return RedirectToAction("","");
+            }
         }
 
         public IActionResult Update(){
