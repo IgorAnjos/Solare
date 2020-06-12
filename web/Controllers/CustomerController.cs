29acb787ad2ea2652dcc8e82b6a4215e076b797e
Igor Anjos <igor.anjos@orbium.com.br>
2020-06-11 23:35:01 -0300

    chore: ajuste para adicionar cliente

--- web/Controllers/CustomerController.cs
+++ web/Controllers/CustomerController.cs
@@ -26,29 +26,64 @@ namespace web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Save(CustomerDTO _customerDTO)
+        public IActionResult Save(CustomerViewModel _customerDTO)
         {
-            
-            if (ModelState.IsValid)
+            try
             {
-                Customer customer = new Customer();
-
-                customer.FullName = _customerDTO.FullName;
-                customer.Type = _customerDTO.Type;
-                customer.AddressBirth = database.AddressBirths.First(i => i.Id == _customerDTO.AddressBirthID);
-                customer.Enable = true;
-                customer.CreatedOn = DateTime.Now;
-
-
-
-                //database.Customers.Add(customer);
-                database.SaveChanges();
+                if (ModelState.IsValid)
+                {
                 
-                return RedirectToAction("","");
+                    #region Informação de Nascimento
+
+                        AddressBirth aBirth = new AddressBirth();
+
+                        aBirth.Id               = _customerDTO.AddressBirth.Id;
+                        aBirth.DateOfBirth      = _customerDTO.AddressBirth.DateOfBirth.ToString();
+                        aBirth.BirthTime        = _customerDTO.AddressBirth.BirthTime.ToString();
+                        aBirth.CityBirth        = _customerDTO.AddressBirth.CityBirth;
+                        aBirth.BirthOfState     = _customerDTO.AddressBirth.BirthOfState; 
+
+                        database.AddressBirths.Add(aBirth);
+                        database.SaveChanges();
+                    #endregion
+
+                    #region Contato
+
+                        Contact contact = new Contact();
+
+                        contact.Id          = _customerDTO.Contact.Id;
+                        contact.Mobile      = _customerDTO.Contact.Mobile;
+                        contact.Telephone   = _customerDTO.Contact.Telephone;
+
+                        database.Contacts.Add(contact);
+                        database.SaveChanges();
+                            
+                    #endregion
+                    
+                    #region Customer
+
+                        Customer customer = new Customer();
+
+                        customer.Id             = _customerDTO.Customer.Id;
+                        customer.FullName       = _customerDTO.Customer.FullName;
+                        customer.Type           = _customerDTO.Customer.Type;
+                        customer.Enable         = true;
+                        customer.CreatedBy      = 1; //teste Igor será usuário logado
+                        customer.CreatedOn      = DateTime.Now;
+                        customer.AddressBirth   = aBirth;
+                        customer.Contact        = contact;
+
+                        database.Customers.Add(customer);
+                        database.SaveChanges();
+
+                    #endregion
+                
+                }
+                return RedirectToAction("Customers","Customer");
             }
-            else
+            catch (System.Exception err)
             {
-                return RedirectToAction("","");
+                throw new Exception(err.Message);
             }
         }
 
