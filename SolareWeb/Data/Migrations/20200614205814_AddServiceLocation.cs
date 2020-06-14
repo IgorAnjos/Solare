5dd9e923efbedb6d60131b7785090102a5574262
Igor Anjos <igor.anjos@orbium.com.br>
2020-06-14 18:28:06 -0300

    chore: add tabela de serviceLocation na migrations

---
+++ SolareWeb/Data/Migrations/20200614205814_AddServiceLocation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SolareWeb.Data.Migrations
+{
+    public partial class AddServiceLocation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServiceLocations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ServiceLocationName = table.Column<string>(nullable: true),
+                    Enable = table.Column<bool>(nullable: false),
+                    CreatedBy = table.Column<int>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    ModifieldBy = table.Column<int>(nullable: false),
+                    ModifieldOn = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceLocations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceLocations");
+        }
+    }
+}
